ada8acf75e8f1cc417bed59cbf36a0e52f42eabb
SouthFox <master@southfox.me>
2023-08-30 17:28:05 +0800

    [hugo/ux] use tailwind css

--- src/main/backend/hugo/site.clj
+++ src/main/backend/hugo/site.clj
@@ -12,11 +12,17 @@
                  (hiccup/html {:escape-strings? false}
                    [:html
                     [:head
-                     [:meta {:name "referrer" :content "no-referrer"}]]
-                    [:body {:style "margin: auto;max-width: 65%;"}
-                     [:div
-                      [:h1 (:title content)]
-                      [:time (:time content)]
-                      [:hr]]
-                     [:div
-                      (:content content)]]]))}))
+                     [:meta {:name "referrer" :content "no-referrer"}]
+                     [:link {:rel "stylesheet" :href "/css/app.css"}]]
+                    [:body
+                     [:div {:class "container p-2 mx-auto"}
+                      [:div {:class "flex flex-row flex-wrap py-4"}
+                       [:div {:class "w-full sm:w-1/3 md:w-1/4 px-2"}
+                        [:div {:class "sticky top-0 p-4 bg-slate-300 rounded-xl w-full"}
+                         [:nav]]]
+                        [:div {:class "w-full sm:w-2/3 md:w-3/4 pt-1 px-2"}
+                         [:h1 (:title content)]
+                         [:time (:time content)]
+                         [:hr]
+                         [:div
+                          (:content content)]]]]]]))}))
