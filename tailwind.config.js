e8fab9e4c7539ce4f6ad9015e6da1919571ce714
SouthFox <master@southfox.me>
2023-09-01 11:25:26 +0800

    [front/ux] add loading element

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/main/frontend/*.cljs'],
+  content: ['./src/main/frontend/*.cljs',
+            './public/*.html'],
   theme: {
     extend: {},
   },
